b40d3ccb3c3560becf1446c1b08c09cc6ed5fd37
Shahbaz-Muzammil <97161768+Shahbaz-Muzammil@users.noreply.github.com>
2025-12-31 23:07:16 +0530

    bla bla bla

---
+++ ModulesAndPackages/package/maths.py
@@ -0,0 +1,6 @@
+def addition (a,b):
+    return a+b
+
+def subtractions(a,b):
+    return a-b
+
---
+++ ModulesAndPackages/test.py
@@ -0,0 +1,5 @@
+from package.maths import addition, subtractions
+from package.subPackages.mult import multiply
+print(addition(5, 3))
+print(subtractions(10, 4))
+print(multiply(5, 3))
